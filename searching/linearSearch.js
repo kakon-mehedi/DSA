a0a1ce47972487727f37bb57f90e5a5d7cada79d
kakon-mehedi <kakon9000@gmail.com>
2023-01-27 19:21:24 +0600

    Add Linear Search Algorithm

--- searching/linearSearch.js
+++ searching/linearSearch.js
@@ -1,1 +1,21 @@
-export const appName = "Linear Search";
+const numbers = [10, 50, 20, 40, 70];
+
+function linearSearch(arr, desiredNumber) {
+  let result = false;
+
+  numbers.map((number, currentIndex) => {
+    if (number === desiredNumber) {
+      result = currentIndex;
+    }
+  });
+
+  return result;
+}
+
+let searchResult = linearSearch(numbers, 10);
+
+if (searchResult) {
+  console.log("Number is in index no " + searchResult);
+} else {
+  console.log("Number not found");
+}
